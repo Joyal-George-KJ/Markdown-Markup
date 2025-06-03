bf543840ad6f49916aa68285042cf24ca1d54e7e
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-06-03 15:00:16 +0530

    Prettier Format and Button marking almost done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,60 +7,179 @@ export default function Home() {
     const InputRef = useRef(null);
     const [md, setMd] = useState("");
 
-    const createMd = (key: string, start: number, end: number, both: boolean): string => {
-        let val: string = '';
-        if ( both && start !== end ) {
+    const createMd = (
+        key: string,
+        start: number,
+        end: number,
+        both: boolean
+    ): string => {
+        let val: string = "";
+        if (both && start !== end) {
             switch (key) {
-                case 'p':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '####### ' + md.slice(start, end) + " #######\n";
+                case "p":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "####### " +
+                        md.slice(start, end) +
+                        " #######\n";
                     break;
-                case 'h1':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '# ' + md.slice(start, end) + " #\n";
+                case "h1":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "# " +
+                        md.slice(start, end) +
+                        " #\n";
                     break;
-                case 'h2':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '## ' + md.slice(start, end) + " ##\n";
+                case "h2":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "## " +
+                        md.slice(start, end) +
+                        " ##\n";
                     break;
-                case 'h3':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '### ' + md.slice(start, end) + " ###\n";
+                case "h3":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "### " +
+                        md.slice(start, end) +
+                        " ###\n";
                     break;
-                case 'h4':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '#### ' + md.slice(start, end) + " ####\n";
+                case "h4":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "#### " +
+                        md.slice(start, end) +
+                        " ####\n";
                     break;
-                case 'h5':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '##### ' + md.slice(start, end) + " #####\n";
+                case "h5":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "##### " +
+                        md.slice(start, end) +
+                        " #####\n";
                     break;
-                case 'h6':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '###### ' + md.slice(start, end) + " ######\n";
+                case "h6":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "###### " +
+                        md.slice(start, end) +
+                        " ######\n";
                     break;
-                case 'q':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '" ' + md.slice(start, end) + ` "\n`;
+                case "q":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        '" ' +
+                        md.slice(start, end) +
+                        ` "\n`;
                     break;
-                case 'i':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '~ ' + md.slice(start, end) + " ~\n";
+                case "i":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "~ " +
+                        md.slice(start, end) +
+                        " ~\n";
                     break;
-                case 'b':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '* ' + md.slice(start, end) + " *\n";
+                case "b":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "* " +
+                        md.slice(start, end) +
+                        " *\n";
                     break;
-                case 'highlight':
-                    val = md.slice(0, start !== 0 ? start - 1: 0) + '=== ' + md.slice(start, end) + " ===\n";
+                case "highlight":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "=== " +
+                        md.slice(start, end) +
+                        " ===\n";
                     break;
-            
+                case "hr":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "_ " +
+                        md.slice(start, end) +
+                        " _\n";
+                    break;
+                case "code":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "` " +
+                        md.slice(start, end) +
+                        " `\n";
+                    break;
+                case "braces":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "``` " +
+                        md.slice(start, end) +
+                        " ```\n";
+                    break;
+                default:
+                    break;
+            }
+        } else if (!both && start !== end) {
+            switch (key) {
+                case "link":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "a: " +
+                        md.slice(start, end) +
+                        " (link)[alt text]\n";
+                    break;
+                case "image":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "img: " +
+                        md.slice(start, end) +
+                        " (Image Link)[alt text]\n";
+                    break;
+                case "ol":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "> " +
+                        md.slice(start, end) +
+                        " \n";
+                    break;
+                case "ul":
+                    val =
+                        md.slice(0, start !== 0 ? start - 1 : 0) +
+                        "- " +
+                        md.slice(start, end) +
+                        " \n";
+                    break;
+
                 default:
                     break;
             }
         } else {
             return md;
         }
-        
+
         return val;
-    }
+    };
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (InputRef.current) {
             let textArea = InputRef.current as HTMLTextAreaElement;
             let start = textArea.selectionStart;
             let end = textArea.selectionEnd;
-            setMd(createMd(e.currentTarget.id, start, end, true));
+            setMd(
+                createMd(
+                    e.currentTarget.id,
+                    start,
+                    end,
+                    [
+                        "p",
+                        "i",
+                        "b",
+                        "highlight",
+                        "q",
+                        "braces",
+                        "code",
+                        "hr",
+                    ].includes(e.currentTarget.id)
+                )
+            );
             console.log(e.currentTarget.id);
         }
     };
@@ -85,7 +204,12 @@ export default function Home() {
                             "ol",
                             "ul",
                         ].map((val, ind) => (
-                            <button key={ind} onClick={handleClick} className={val} id={val}>
+                            <button
+                                key={ind}
+                                onClick={handleClick}
+                                className={val}
+                                id={val}
+                            >
                                 <Image
                                     src={require(`../public/Image/${val}.svg`)}
                                     alt={`Icon ${val}`}
@@ -94,12 +218,12 @@ export default function Home() {
                         ))}
                     </div>
                     <div>
-                        <button onClick={handleClick} className='preview'>
-                                <Image
-                                    src={require(`../public/Image/play-fill.svg`)}
-                                    alt={`Icon preview`}
-                                />
-                            </button>
+                        <button onClick={handleClick} className="preview">
+                            <Image
+                                src={require(`../public/Image/play-fill.svg`)}
+                                alt={`Icon preview`}
+                            />
+                        </button>
                     </div>
                 </div>
 
