61734f73f5df565a69fbe8f62c040add7a294557
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-06-14 19:27:56 +0530

    spear movement made

---
+++ src/app/EffectProvider.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+type EffectProp = {
+    height: number;
+    width: number;
+    count: number;
+    direction?:
+        | "none"
+        | "left"
+        | "right"
+        | "up"
+        | "down"
+        | "upleft"
+        | "upright"
+        | "downright"
+        | "downleft"
+        | "random";
+    speed?: number;
+};
+
+type Flake = {
+    x: number;
+    y: number;
+    r: number;
+    direction:
+        | "none"
+        | "left"
+        | "right"
+        | "up"
+        | "down"
+        | "upleft"
+        | "upright"
+        | "downright"
+        | "downleft"
+        | "random";
+};
+
+export default function EffectProvider({
+    height,
+    width,
+    count,
+    direction,
+    speed,
+}: EffectProp) {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const flakesRef = useRef<Flake[]>([]);
+
+    useEffect(() => {
+        if (height === 0 && width === 0) return;
+
+        const myCanvas = canvasRef.current;
+        if (!myCanvas) return;
+
+        myCanvas.height = height;
+        myCanvas.width = width;
+
+        const ctrl = myCanvas.getContext("2d");
+        if (!ctrl) return;
+
+        const positionPicker = () => {
+            for (let i = 0; i < count; i++) {
+                let [x, y, r] = [
+                    Math.floor(Math.random() * width),
+                    Math.floor(Math.random() * height),
+                    Math.floor(Math.random() * 5),
+                ];
+
+                if (x <= width && y <= height) {
+                    console.log(i, x, y, r);
+                    flakesRef.current.push({
+                        x: x,
+                        y: y,
+                        r: r,
+                        direction: direction ? direction : "left",
+                    });
+                } else {
+                    --i;
+                }
+            }
+        };
+        positionPicker();
+
+        const drawShape = () => {
+            ctrl.clearRect(0, 0, width, height);
+            flakesRef.current.map((val) => {
+                ctrl.beginPath();
+                ctrl.arc(val.x, val.y, val.r, 0, 2 * Math.PI);
+                ctrl.fillStyle = "white";
+                ctrl.fill();
+                ctrl.closePath();
+            });
+        };
+        drawShape();
+
+        const moveShape = () => {
+            flakesRef.current.map((val) => {
+                switch (val.direction) {
+                    case "left":
+                        val.x -= speed ? speed : 0.1;
+                        if (val.x < 0) {
+                            val.x = width;
+                            val.y = Math.floor(Math.random() * height);
+                        }
+                        break;
+                    case "right":
+                        val.x += speed ? speed : 0.1;
+                        if (val.x > width) {
+                            val.x = 0;
+                            val.y = Math.floor(Math.random() * height);
+                        }
+                        break;
+                    case "up":
+                        val.y -= speed ? speed : 0.1;
+                        if (val.y < 0) {
+                            val.y = height;
+                            val.x = Math.floor(Math.random() * height);
+                        }
+                        break;
+                    case "down":
+                        val.y += speed ? speed : 0.1;
+                        if (val.y > height) {
+                            val.y = 0;
+                            val.x = Math.floor(Math.random() * height);
+                        }
+                        break;
+                    case "upleft":
+                        val.x -= speed ? speed : 0.1;
+                        val.y -= speed ? speed : 0.1;
+                        if (val.y < 0 || val.x < 0) {
+                            val.y = height;
+                            val.x = Math.floor(Math.random() * width);
+                        }
+                        break;
+                    case "upright":
+                        val.x += speed ? speed : 0.1;
+                        val.y += speed ? speed : 0.1;
+                        if (val.y > height || val.x > width) {
+                            val.x = Math.floor(
+                                Math.random() * width - width / 2
+                            );
+                            val.y = Math.floor(
+                                Math.random() * height - height / 2
+                            );
+                        }
+                        break;
+                    case "downleft":
+                        val.x -= speed ? speed : 0.1;
+                        val.y += speed ? speed : 0.1;
+                        if (val.y > height || val.x < 0) {
+                            val.x = Math.floor(Math.random() * width);
+                            val.y = Math.floor(
+                                Math.random() * height - height / 2
+                            );
+                        }
+                        break;
+                    case "downright":
+                        val.x += speed ? speed : 0.1;
+                        val.y -= speed ? speed : 0.1;
+                        if (val.x > width || val.y < 0) {
+                            val.y = Math.floor(Math.random() * height);
+                            val.x = Math.floor(Math.random() * width);
+                        }
+                        break;
+                    case "none":
+                        break;
+
+                    default:
+                        break;
+                }
+
+                drawShape();
+            });
+            requestAnimationFrame(moveShape);
+        };
+        moveShape();
+    }, [width, height]);
+
+    return (
+        <canvas
+            ref={canvasRef}
+            style={{ display: "block", backgroundColor: "black" }}
+        />
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import InputMd from "./InputMd";
+"use client";
 
-export default function page() {
+import { useEffect, useState } from "react";
+import EffectProvider from "./EffectProvider";
 
-  return (
-    <div>
-      <InputMd />
-    </div>
-  )
+function page() {
+    let [sendProps, setSendProps] = useState({ height: 0, width: 0 });
+
+    useEffect(() => {
+        let handleResizing = () => {
+            setSendProps((val) => {
+                return { ...val, height: innerHeight, width: innerWidth };
+            });
+        };
+
+        handleResizing();
+
+        window.addEventListener("resize", handleResizing);
+
+        return window.removeEventListener("resize", handleResizing);
+    }, []);
+
+    return (
+        <div>
+            <EffectProvider {...sendProps} count={50} direction="left" speed={1} />
+        </div>
+    );
 }
+
+export default page;
