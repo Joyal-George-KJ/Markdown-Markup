ec9687d3c249a3dfc87965693f663ea29f21053b
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-06-16 20:12:26 +0530

    Implimentation of the Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import EffectProvider from "./EffectProvider";
+import EffectProvider, { DataType } from "./EffectProvider";
+import ControlBar from "./ControlBar";
 
 function page() {
-    let [sendProps, setSendProps] = useState({ height: 0, width: 0 });
+    const [sendProps, setSendProps] = useState({ height: 0, width: 0 });
+    const [toggle, setToggle] = useState<boolean>(false);
+    const [data, setData] = useState<DataType>({
+        count: 50,
+        direction: "left",
+        speed: 0.1,
+        flakeColor: "#000000",
+        bgColor: "#ffffff",
+    });
 
     useEffect(() => {
+        console.log(data);
+
         let handleResizing = () => {
             setSendProps((val) => {
                 return { ...val, height: innerHeight, width: innerWidth };
@@ -18,11 +29,38 @@ function page() {
         window.addEventListener("resize", handleResizing);
 
         return window.removeEventListener("resize", handleResizing);
-    }, []);
+    }, [data]);
 
     return (
-        <div>
-            <EffectProvider {...sendProps} count={50} direction="left" speed={1} />
+        <div style={{ position: "relative" }}>
+            {toggle ? (
+                <ControlBar toggler={() => {setToggle(false)}} data={data} setData={setData} />
+            ) : (
+                <button className="panel" onClick={() => {setToggle(true)}}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-list"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                        />
+                    </svg>
+                </button>
+            )}
+            <EffectProvider
+                height={sendProps.height}
+                width={sendProps.width}
+                count={data.count}
+                direction={data.direction}
+                speed={data.speed}
+                bgColor={data.bgColor}
+                flakeColor={data.flakeColor}
+            ></EffectProvider>
         </div>
     );
 }
